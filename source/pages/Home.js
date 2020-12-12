af9a0a75eff0f39788e7e9e5a4958026ac5904b1
hagantabrema <hagantabangun@gmail.com>
2020-12-12 08:38:12 +0700

    updated icons

--- source/pages/Home.js
+++ source/pages/Home.js
@@ -1,14 +1,30 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import React, { useState } from 'react'
+import { StyleSheet, Text, View, TextInput } from 'react-native'
+import { ACTIVE_COLOR, INACTIVE_COLOR } from '../utils/constant'
 
 const Home = () => {
+    const [text, setText] = useState('');
     return (
         <View>
-            <Text>Home</Text>
+            <View style={styles.input}>
+                <TextInput
+                    placeholder="Cari gejala penyakit"
+                    onChangeText={text => setText(text)}
+                    defaultValue={text}
+                />
+            </View>
         </View>
     )
 }
 
 export default Home
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    input: {
+        width: 320,
+        height: 46,
+        alignItems: 'center',
+        borderRadius: 5,
+        backgroundColor: ACTIVE_COLOR
+    }
+})
